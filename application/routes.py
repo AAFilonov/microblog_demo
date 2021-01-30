27b07420a6d96059e4d71eea2518a9051ee75d86
filon13p <filon13p@gmail.com>
2021-01-30 17:04:22 +0300

    xчето

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import *
 from application import app
+from application.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -21,6 +23,16 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/user')
